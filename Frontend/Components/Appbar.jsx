0a6b1372c1443e62163f02d74f9da1541ec2e745
Ritesh Kriplani <kriplanira@rknec.edu>
2025-02-09 02:16:18 +0530

    Create Appbar.jsx

---
+++ Frontend/Components/Appbar.jsx
@@ -0,0 +1,18 @@
+export function Appbar(){
+  return <div className="shadow h-14 flex justify-between">
+    <div className="flex justify-center flex-col ml-2 h-full">
+    PayTM App
+    </div>
+    <div className="flex">
+        <div className="flex flex-col justify-center h-full mr-4">
+            Hello
+        </div>
+        <div className="rounded-full flex border h-12 w-12 justify-center text-xl bg-slate-200 mt-1 mr-2">
+            <div className="flex justify-center flex-col h-full">
+                U
+            </div>
+        </div>
+    </div>
+
+   </div>
+}
